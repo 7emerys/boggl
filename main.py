420499f7d58ed185472559baa1a8706ed81e577e
7emerys <arsenyi2017431003@gmail.com>
2024-04-09 04:27:20 +0300

    Изменение кнопок фона, шрифта и доработка случайного выбора букв матрицы

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import sys
 import random
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QGridLayout, QLabel, QVBoxLayout
-from PyQt5.QtMultimedia import QSound
-
+from PyQt5.QtWidgets import *
+from PyQt5.QtMultimedia import *
+from PyQt5.QtCore import *
 
 
 class MW(QMainWindow):
-
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Боггл")
+        self.setMinimumSize(300, 500)
 
         layout = QVBoxLayout()
+
         self.start_button_layout = QVBoxLayout()
         self.rules_button_layout = QVBoxLayout()
         self.quit_button_layout = QVBoxLayout()
@@ -22,20 +23,24 @@ class MW(QMainWindow):
         self.start_button.setFixedSize(150, 50)
         self.start_button_layout.addWidget(self.start_button)
 
+
         self.rules_button = QPushButton("Правила", self)
         self.rules_button.clicked.connect(self.open_Rules)
         self.rules_button.setFixedSize(150, 50)
         self.rules_button_layout.addWidget(self.rules_button)
+
 
         self.sound_button = QPushButton("Звук", self)
         self.sound_button.clicked.connect(self.open_Sounds)
         self.sound_button.setFixedSize(150, 50)
         self.sound_button_layout.addWidget(self.sound_button)
 
+
         self.quit_button = QPushButton("Выход", self)
         self.quit_button.clicked.connect(self.close)
         self.quit_button.setFixedSize(150, 50)
         self.quit_button_layout.addWidget(self.quit_button)
+
 
         layout.addLayout(self.start_button_layout)
         layout.addLayout(self.rules_button_layout)
@@ -43,17 +48,36 @@ class MW(QMainWindow):
         layout.addLayout(self.quit_button_layout)
 
         layout.setSpacing(20)
-        layout.setContentsMargins(100, 100, 100, 100)
+        layout.setAlignment(Qt.AlignCenter)
 
         widget = QWidget()
         widget.setLayout(layout)
         self.setCentralWidget(widget)
 
+        self.setStyleSheet("background-color: #79D6F4;")
+
+        button_style = """
+                                QPushButton {
+                                    font-size: 18px;
+                                    border: 2px solid #8f8f8f;
+                                    border-radius: 10px;
+                                    padding: 10px;
+                                    background-color: #d7d7d7;
+                                }
+                                QPushButton:hover {
+                                    background-color: #7FB6C3;
+                                }
+                            """
+        self.start_button.setStyleSheet(button_style)
+        self.rules_button.setStyleSheet(button_style)
+        self.sound_button.setStyleSheet(button_style)
+        self.quit_button.setStyleSheet(button_style)
+
+
     def open_Wgame(self):
         self.game_window = Wgame()
         self.game_window.show()
         self.close()
-
 
     def open_Rules(self):
         self.rule_window = Rules()
@@ -65,80 +89,236 @@ class MW(QMainWindow):
         self.sound_window.show()
         self.close()
 
-class Wgame(QWidget):
+
+class Wgame(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Боггл")
-        self.resize(1000, 450)
+        self.setMinimumSize(800, 400)
 
         self.board = self.matrix()
-        self.init_ui()
 
-    def init_ui(self):
-        layout = QGridLayout()
+
+        buttons_layout = QVBoxLayout()
+        matrix_layuot = QGridLayout()
+
+        self.help_button_layout = QVBoxLayout()
+        self.back_button_layout = QVBoxLayout()
+
+
+        self.back_button = QPushButton("Назад", self)
+        self.back_button.clicked.connect(self.comeback)
+        self.back_button.setFixedSize(100, 30)
+        self.back_button_layout.addWidget(self.back_button)
+
+        self.help_button = QPushButton("Подсказка", self)
+        self.help_button.clicked.connect(self.podskazat)
+        self.help_button.setFixedSize(100, 30)
+        self.help_button_layout.addWidget(self.help_button)
+
+
+        buttons_layout.addLayout(self.help_button_layout)
+        buttons_layout.addLayout(self.back_button_layout)
+
+
 
         self.labels = []
         for i in range(4):
             for j in range(4):
                 btn = QPushButton(self.board[i][j])
                 btn.clicked.connect(self.btn_cl)
-                layout.addWidget(btn, i, j)
+                btn.setStyleSheet("""
+                    QPushButton {
+                        font-size: 15px;
+                        border: 2px solid #8f8f8f;
+                        border-radius: 10px;
+                        padding: 5px;
+                        background-color: #d7d7d7;
+                    }
+                    QPushButton:hover {
+                        background-color: #7FB6C3;
+                    }
+                """)
+                matrix_layuot.addWidget(btn, i, j)
                 self.labels.append(btn)
-
         self.current_letter_label = QLabel("")
-        layout.addWidget(self.current_letter_label, 5, 0, 1, 4)
+        matrix_layuot.addWidget(self.current_letter_label, 5, 0, 1, 4)
 
-        self.setLayout(layout)
+
+        btn_matrix = QVBoxLayout()
+
+        btn_matrix.addLayout(matrix_layuot)
+        btn_matrix.addLayout(buttons_layout)
+
+        widget = QWidget()
+        widget.setLayout(btn_matrix)
+        self.setCentralWidget(widget)
+
+        self.setStyleSheet("background-color: #79D6F4;")
+        button_style = """
+                                QPushButton {
+                                    font-size: 15px;
+                                    border: 2px solid #8f8f8f;
+                                    border-radius: 10px;
+                                    padding: 5px;
+                                    background-color: #d7d7d7;
+                                }
+                                QPushButton:hover {
+                                    background-color: #7FB6C3;
+                                }
+                            """
+
+        self.help_button.setStyleSheet(button_style)
+        self.back_button.setStyleSheet(button_style)
+
 
     def matrix(self):
         letters = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
         board = []
-        for _ in range(4):
-            row = [random.choice(letters) for _ in range(4)]
+        row_letters = random.sample(letters, k = 16)
+        for i in range(0, 16, 4):
+            row = row_letters[i:i + 4]
             board.append(row)
         return board
+
 
     def btn_cl(self):
         sender = self.sender()
         text = sender.text()
         self.current_letter_label.setText(text)
 
-        podskazka = QPushButton("Подсказать", self)
-        #self.podskazka.clicked.connect дописать то что будет происходить
-        podskazka.move(900, 400)
+
+    def podskazat(self):
+        pass
+
+    def comeback(self):
+        self.menu = MW()
+        self.menu.show()
+        self.close()
 
 
-class Rules(QWidget):
+class Rules(QMainWindow):
     def __init__(self):
         super().__init__()
-        layout = QVBoxLayout()
         self.setWindowTitle("Правила")
-        self.label = QLabel("Правила игры:\nВ начале выводится матрица 4*4, заполненная буквами русского алфавита.\nПользователь должен вписывать в поле ответа слово, состоящее из букв, которые есть в матрице.\nОчки зависят от длины и количества слов. За каждую букву начисляется 1 очко.\nВ игре есть кнопка подсказки слова, при нажатии на которую пользователю выводится подходящее слово.\nОчки за такое слово начисляются с коэффициентом 0,5, кнопка будет доступна 3 раза за 1 игру",self)
+        self.setMinimumSize(200, 300)
+
+        layout = QVBoxLayout()
+
+        self.label = QLabel("Правила игры:\nВ начале выводится матрица 4*4, заполненная буквами русского алфавита.\nПользователь должен вписывать в поле ответа слово, состоящее из букв, которые есть в матрице.\nОчки зависят от длины и количества слов. За каждую букву начисляется 1 очко.\nВ игре есть кнопка подсказки слова, при нажатии на которую пользователю выводится подходящее слово.\nОчки за такое слово начисляются с коэффициентом 0,5, кнопка будет доступна 3 раза за 1 игру", self)
         self.label.setStyleSheet("""
         font-size:20px;
         """)
-        layout.addWidget(self.label)
-        self.setLayout(layout)
 
-class Sounds(QWidget):
+        self.back_button_layout = QVBoxLayout()
+        self.back_button = QPushButton("Назад", self)
+        self.back_button.clicked.connect(self.comeback)
+        self.back_button.setFixedSize(100, 30)
+        self.back_button_layout.addWidget(self.back_button)
+
+
+        layout.addWidget(self.label)
+        layout.addLayout(self.back_button_layout)
+
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+        self.setStyleSheet("background-color: #79D6F4;")
+        button_style = """
+                                        QPushButton {
+                                            font-size: 15px;
+                                            border: 2px solid #8f8f8f;
+                                            border-radius: 10px;
+                                            padding: 5px;
+                                            background-color: #d7d7d7;
+                                        }
+                                        QPushButton:hover {
+                                            background-color: #7FB6C3;
+                                        }
+                                    """
+        self.back_button.setStyleSheet(button_style)
+
+    def comeback(self):
+        self.menu = MW()
+        self.menu.show()
+        self.close()
+
+
+class Sounds(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Звук")
-        self.resize(1000, 450)
+        self.setMinimumSize(300, 500)
 
-        self.sound = QSound("song.wav", self)
+        self.Music()
+
+        layout = QVBoxLayout()
+
+        self.music_play_button_layout = QVBoxLayout()
+        self.music_stop_button_layout = QVBoxLayout()
+
+        self.back_button_layout = QVBoxLayout()
+        self.back_button = QPushButton("Назад", self)
+        self.back_button.clicked.connect(self.comeback)
+        self.back_button.setFixedSize(150, 50)
+        self.back_button_layout.addWidget(self.back_button)
 
         self.music_play_button = QPushButton("Включить музыку", self)
-        self.music_play_button.clicked.connect(self.sound.play)
-        self.music_play_button.move(450, 100)
+        self.music_play_button.clicked.connect(self.sound_play)
+        self.music_play_button.setFixedSize(150, 50)
+        self.music_play_button_layout.addWidget(self.music_play_button)
 
         self.music_stop_button = QPushButton("Остановить музыку", self)
-        self.music_stop_button.clicked.connect(self.sound.stop)
-        self.music_stop_button.move(450, 200)
+        self.music_stop_button.clicked.connect(self.sound_stop)
+        self.music_stop_button.setFixedSize(150, 50)
+        self.music_stop_button_layout.addWidget(self.music_stop_button)
+
+        layout.addLayout(self.music_play_button_layout)
+        layout.addLayout(self.music_stop_button_layout)
+        layout.addLayout(self.back_button_layout)
+
+        layout.setSpacing(20)
+        layout.setAlignment(Qt.AlignCenter)
+
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+        self.setStyleSheet("background-color: #79D6F4;")
+        button_style = """
+                                                QPushButton {
+                                                    font-size: 15px;
+                                                    border: 2px solid #8f8f8f;
+                                                    border-radius: 10px;
+                                                    padding: 5px;
+                                                    background-color: #d7d7d7;
+                                                }
+                                                QPushButton:hover {
+                                                    background-color: #7FB6C3;
+                                                }
+                                            """
+        self.music_stop_button.setStyleSheet(button_style)
+        self.music_play_button.setStyleSheet(button_style)
+        self.back_button.setStyleSheet(button_style)
+
+    def Music(self):
+        self.sound = QSound("song.wav", self)
+
+    def sound_play(self):
+        self.sound.play()
+
+    def sound_stop(self):
+        self.sound.stop()
+
+    def comeback(self):
+        self.menu = MW()
+        self.menu.show()
+        self.close()
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     menu = MW()
     menu.show()
     sys.exit(app.exec())
-
--- test.py
+++ test.py
@@ -1,37 +1,216 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton
+import sys
+import random
+from PyQt5.QtWidgets import *
+from PyQt5.QtMultimedia import *
+from PyQt5.QtCore import *
 
 
-from PyQt5 import QtWidgets
+class MW(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Боггл")
+        self.setMinimumSize(300, 500)
 
-def button_click(button_num):
-    print(f"Нажата кнопка {button_num}")
+        layout = QVBoxLayout()
 
-app = QtWidgets.QApplication([])
-window = QtWidgets.QWidget()
-window.setWindowTitle("Пример кнопок")
+        self.start_button_layout = QVBoxLayout()
+        self.rules_button_layout = QVBoxLayout()
+        self.quit_button_layout = QVBoxLayout()
+        self.sound_button_layout = QVBoxLayout()
 
-# Создание 4 кнопок
-button1 = QtWidgets.QPushButton("Кнопка 1")
-button1.clicked.connect(lambda: button_click(1))
-button2 = QtWidgets.QPushButton("Кнопка 2")
-button2.clicked.connect(lambda: button_click(2))
-button3 = QtWidgets.QPushButton("Кнопка 3")
-button3.clicked.connect(lambda: button_click(3))
-button4 = QtWidgets.QPushButton("Кнопка 4")
-button4.clicked.connect(lambda: button_click(4))
+        self.start_button = QPushButton("Начало игры", self)
+        self.start_button.clicked.connect(self.open_Wgame)
+        self.start_button.setFixedSize(150, 50)
+        self.start_button_layout.addWidget(self.start_button)
+        self.start_button.setStyleSheet("""
+                font-size:20px;
+                """)
 
-# Создание вертикального и горизонтального размещения для кнопок
-v_layout = QtWidgets.QVBoxLayout()
-h_layout = QtWidgets.QHBoxLayout()
-h_layout.addWidget(button1)
-h_layout.addWidget(button2)
-v_layout.addLayout(h_layout)
-h_layout = QtWidgets.QHBoxLayout()
-h_layout.addWidget(button3)
-h_layout.addWidget(button4)
-v_layout.addLayout(h_layout)
+        self.rules_button = QPushButton("Правила", self)
+        self.rules_button.clicked.connect(self.open_Rules)
+        self.rules_button.setFixedSize(150, 50)
+        self.rules_button_layout.addWidget(self.rules_button)
+        self.rules_button.setStyleSheet("""
+                font-size:20px;
+                """)
 
-window.setLayout(v_layout)
-window.show()
+        self.sound_button = QPushButton("Звук", self)
+        self.sound_button.clicked.connect(self.open_Sounds)
+        self.sound_button.setFixedSize(150, 50)
+        self.sound_button_layout.addWidget(self.sound_button)
+        self.sound_button.setStyleSheet("""
+        font-size:20px;
+        """)
 
-app.exec()
+        self.quit_button = QPushButton("Выход", self)
+        self.quit_button.clicked.connect(self.close)
+        self.quit_button.setFixedSize(150, 50)
+        self.quit_button_layout.addWidget(self.quit_button)
+        self.quit_button.setStyleSheet("""
+                font-size:20px;
+                """)
+
+        layout.addLayout(self.start_button_layout)
+        layout.addLayout(self.rules_button_layout)
+        layout.addLayout(self.sound_button_layout)
+        layout.addLayout(self.quit_button_layout)
+
+        layout.setSpacing(20)
+        layout.setAlignment(Qt.AlignCenter)
+
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+    def open_Wgame(self):
+        self.game_window = Wgame()
+        self.game_window.show()
+        self.close()
+
+    def open_Rules(self):
+        self.rule_window = Rules()
+        self.rule_window.show()
+        self.close()
+
+    def open_Sounds(self):
+        self.sound_window = Sounds()
+        self.sound_window.show()
+        self.close()
+
+
+class Wgame(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Боггл")
+        self.setMinimumSize(600, 800)
+
+        layout = QVBoxLayout()
+
+        self.podskazka_button_layout = QVBoxLayout()
+        self.back_button_layout = QVBoxLayout()
+
+        self.back_button = QPushButton("Назад", self)
+        self.back_button.clicked.connect(self.comeback)
+        self.back_button.setFixedSize(150, 50)
+        self.back_button_layout.addWidget(self.back_button)
+        self.back_button.setStyleSheet("""
+                                font-size:20px;
+                                """)
+
+        self.podskazka_button = QPushButton("Подсказка", self)
+        self.podskazka_button.clicked.connect(self.podskazat)
+        self.podskazka_button.setFixedSize(150, 50)
+        self.podskazka_button_layout.addWidget(self.podskazka_button)
+        self.podskazka_button.setStyleSheet("""
+                        font-size:20px;
+                        """)
+
+        layout.addLayout(self.podskazka_button_layout)
+        layout.addLayout(self.back_button_layout)
+
+        layout.setSpacing(20)
+        layout.setAlignment(Qt.AlignCenter)
+
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+    def podskazat(self):
+        pass
+
+    def comeback(self):
+        self.menu = MW()
+        self.menu.show()
+        self.close()
+
+
+class Rules(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Правила")
+        self.setMinimumSize(200, 300)
+
+        layout = QVBoxLayout()
+
+        self.label = QLabel("Правила игры:\nВ начале выводится матрица 4*4, заполненная буквами русского алфавита.\nПользователь должен вписывать в поле ответа слово, состоящее из букв, которые есть в матрице.\nОчки зависят от длины и количества слов. За каждую букву начисляется 1 очко.\nВ игре есть кнопка подсказки слова, при нажатии на которую пользователю выводится подходящее слово.\nОчки за такое слово начисляются с коэффициентом 0,5, кнопка будет доступна 3 раза за 1 игру", self)
+        self.label.setStyleSheet("""
+        font-size:20px;
+        """)
+
+        self.back_button_layout = QVBoxLayout()
+        self.back_button = QPushButton("Назад", self)
+        self.back_button.clicked.connect(self.comeback)
+        self.back_button.setFixedSize(150, 50)
+        self.back_button_layout.addWidget(self.back_button)
+        self.back_button.setStyleSheet("""
+                                        font-size:20px;
+                                        """)
+
+        layout.addWidget(self.label)
+        layout.addLayout(self.back_button_layout)
+
+        layout.setSpacing(20)
+        layout.setAlignment(Qt.AlignCenter)
+
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+    def comeback(self):
+        self.menu = MW()
+        self.menu.show()
+        self.close()
+
+
+class Sounds(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Звук")
+        self.setMinimumSize(300, 600)
+
+        layout = QVBoxLayout()
+
+        self.music_play_button_layout = QVBoxLayout()
+        self.music_stop_button_layout = QVBoxLayout()
+
+        self.music_play_button = QPushButton("Включить музыку", self)
+        self.music_play_button.clicked.connect(self.sound_play)
+        self.music_play_button.setFixedSize(150, 50)
+        self.music_play_button_layout.addWidget(self.music_play_button)
+        self.music_play_button.setStyleSheet("""
+                                        font-size:20px;
+                                        """)
+
+        self.music_stop_button = QPushButton("Остановить музыку", self)
+        self.music_stop_button.clicked.connect(self.sound_stop)
+        self.music_stop_button.setFixedSize(150, 50)
+        self.music_stop_button_layout.addWidget(self.music_stop_button)
+        self.music_stop_button.setStyleSheet("""
+                                        font-size:20px;
+                                        """)
+
+        layout.addLayout(self.music_play_button_layout)
+        layout.addLayout(self.music_stop_button_layout)
+
+        layout.setSpacing(20)
+        layout.setAlignment(Qt.AlignCenter)
+
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+    def Music(self):
+        self.sound = QSound("song.wav", self)
+
+    def sound_play(self):
+        self.sound.play()
+
+    def sound_stop(self):
+        self.sound.stop()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    menu = MW()
+    menu.show()
+    sys.exit(app.exec())
